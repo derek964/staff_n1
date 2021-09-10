eff13859410b17da910c8576ed34c423a7241db5
derek964 <zhaoboliang964@163.com>
2021-09-10 10:56:10 +0800

    添加url，添加views，添加导航页，添加html各个页面

--- staff/views.py
+++ staff/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+    return render(request, 'index.html')
+
+def stat_staffinfo(request):
+    return render(request, 'stat_staffinfo.html')
+
+def stat_projwork(request):
+    return render(request, 'stat_projwork.html', context=locals())
+
+def staff_hire(request):
+    return render(request, 'staff_hire.html', context=locals())
+
+def staff_hcmodify(request):
+    return render(request, 'staff_hcmodify.html', context=locals())
+
+def staff_percent(request):
+    return render(request, 'staff_percent.html', context=locals())
+
+
--- staff_n1/settings.py
+++ staff_n1/settings.py
@@ -38,7 +38,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'staff.apps.StaffConfig',
-    'staff',
 ]
 
 MIDDLEWARE = [
